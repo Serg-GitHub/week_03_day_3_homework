e172f6bf7d77fea41d96645c9675358588e6d9ee
Sergio <sergio.eneide@protonmail.com>
2021-06-30 23:14:15 +0100

    second push with links and css

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,12 @@
+from flask import render_template
 from app import app
-from flask import render_template # ADDED
 from models.items import order
+from models.items import order_1
 
 @app.route('/')
 def index():
     return render_template('index.html', title='My Order List', order=order)
+
+@app.route('/order')
+def orders():
+    return render_template('order_1.html', title='My Second Order', order_1=order_1)
--- models/items.py
+++ models/items.py
@@ -4,5 +4,7 @@ from models.order import *
 
 
 item1 = Order("Sergio", "2021/06/30", "2", "Lord of the rings", "Fantasy book")
-item2 = Order("Arek", "2021/06/29", "3", "James Bond", "Action book")
+item2 = Order("Arek", "2021/06/30", "3", "James Bond", "Action book")
+item3 = Order("Simon", "2021/06/30", "1", "Cold Granite", "Crime Book")
 order = [item1, item2]
+order_1 = [item3]

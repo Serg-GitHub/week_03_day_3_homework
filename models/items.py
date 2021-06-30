1b6f1573f77f1a801b85d690318746ae0faaa42f
Sergio <sergio.eneide@protonmail.com>
2021-07-01 00:24:47 +0100

    final push with css and images

--- models/items.py
+++ models/items.py
@@ -6,5 +6,6 @@ from models.order import *
 item1 = Order("Sergio", "2021/06/30", "2", "Lord of the rings", "Fantasy book")
 item2 = Order("Arek", "2021/06/30", "3", "James Bond", "Action book")
 item3 = Order("Simon", "2021/06/30", "1", "Cold Granite", "Crime Book")
+item4 = Order("Akwasi", "2021/06/30", "1", "The Zombie Survival Guide", "Non Fiction")
 order = [item1, item2]
-order_1 = [item3]
+order_1 = [item3,item4]
